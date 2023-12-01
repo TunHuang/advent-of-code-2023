620f462c3a9aa9920b81ba81a4eaab3ab1dfe809
Tun Huang <huang.tun.anton@gmail.com>
2023-12-01 23:01:27 +0100

    Shorten day-01 code with a mapping object

--- day-01/index.js
+++ day-01/index.js
@@ -21,32 +21,32 @@
     console.log(calibrationSum);
     
     // Part 2
-    
+
+    const letterDigitMapping = {
+      one: '1',
+      two: '2',
+      three: '3',
+      four: '4',
+      five: '5',
+      six: '6',
+      seven: '7',
+      eight: '8',
+      nine: '9'
+    };
+
     const findFirstDigit_2 = string => {
       if (!isNaN(string[0])) return string[0];
-      if (string.startsWith('one')) return '1';
-      if (string.startsWith('two')) return '2';
-      if (string.startsWith('three')) return '3';
-      if (string.startsWith('four')) return '4';
-      if (string.startsWith('five')) return '5';
-      if (string.startsWith('six')) return '6';
-      if (string.startsWith('seven')) return '7';
-      if (string.startsWith('eight')) return '8';
-      if (string.startsWith('nine')) return '9';
+      for (const key in letterDigitMapping) {
+        if (string.startsWith(key)) return letterDigitMapping[key];
+      }
       return findFirstDigit_2(string.slice(1));
     };
-    
+
     const findLastDigit_2 = string => {
       if (!isNaN(string.at(-1))) return string.at(-1);
-      if (string.endsWith('one')) return '1';
-      if (string.endsWith('two')) return '2';
-      if (string.endsWith('three')) return '3';
-      if (string.endsWith('four')) return '4';
-      if (string.endsWith('five')) return '5';
-      if (string.endsWith('six')) return '6';
-      if (string.endsWith('seven')) return '7';
-      if (string.endsWith('eight')) return '8';
-      if (string.endsWith('nine')) return '9';
+      for (const key in letterDigitMapping) {
+        if (string.endsWith(key)) return letterDigitMapping[key];
+      }
       return findLastDigit_2(string.slice(0, -1));
     };
     
